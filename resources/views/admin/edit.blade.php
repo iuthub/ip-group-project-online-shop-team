552d7e8c9aef41a7fc63d79c9d2f4ef7a16209b5
Jasurkhon <jasur.uzb10@gmail.com>
2020-05-10 20:50:00 +0500

    fixed mini problems

--- resources/views/admin/edit.blade.php
+++ resources/views/admin/edit.blade.php
@@ -3,27 +3,51 @@
 
 
 <div class="container">
-	
+	@if(Session::has('success'))
+      <div class="alert alert-success text-center">
+      {{ Session::get('success') }}
+      </div>
+  @endif
 <form action="{{ route('product.edited') }}" method="post" enctype="multipart/form-data">
 	@csrf
   <div class="form-group">
     <label for="exampleInputEmail1">Product name</label>
-    <input type="text" class="form-control" id="exampleInputEmail1"name="name" value="{{ $product->name }}">
+    <input type="text" class="form-control @error('name') is-invalid @enderror" id="exampleInputEmail1"name="name" value="{{ $product->name }}">
+    @error('name')
+      <span class="invalid-feedback" role="alert">
+          <strong>{{ $message }}</strong>
+      </span>
+  @enderror
    
   </div>
 
   <div class="form-group">
     <label for="exampleInputPrice1">Price</label>
-    <input type="text" class="form-control" id="exampleInputPrice1" name="price" value="{{ $product->price }}">
+    <input type="text" class="form-control @error('price') is-invalid @enderror" id="exampleInputPrice1" name="price" value="{{ $product->price }}">
+    @error('price')
+      <span class="invalid-feedback" role="alert">
+          <strong>{{ $message }}</strong>
+      </span>
+  @enderror
  </div>
   
   <div class="form-group">
     <label for="exampleFormControlFile1">Image</label>
-    <input type="file" class="form-control-file" id="exampleFormControlFile1" name="image" value="{{ $product->image}}">
+    <input type="file" class="form-control-file @error('image') is-invalid @enderror" id="exampleFormControlFile1" name="image" value="{{ $product->image}}">
+    @error('image')
+      <span class="invalid-feedback" role="alert">
+          <strong>{{ $message }}</strong>
+      </span>
+  @enderror
   </div>
   <div class="form-group">
     <label for="exampleInputDescription">Description</label>
-    <input type="textarea" class="form-control" id="exampleInputDescription" name="description" height="400"value="{{ $product->description }}">
+    <input type="textarea" class="form-control @error('description') is-invalid @enderror" id="exampleInputDescription" name="description" height="400"value="{{ $product->description }}">
+    @error('description')
+      <span class="invalid-feedback" role="alert">
+          <strong>{{ $message }}</strong>
+      </span>
+  @enderror
  </div>
  @csrf
    <input type="hidden" name="id" value="{{ $productId }}">
--- resources/views/product/checkout.blade.php
+++ resources/views/product/checkout.blade.php
@@ -2,6 +2,11 @@
 
 @section('content')
 <div class="container">
+	 @if(Session::has('success'))
+                <div class="alert alert-success text-center">
+                {{ Session::get('success') }}
+                </div>
+            @endif
 	<strong><h1  class=" text-center text-secondary">Checkout</h1></strong>
 	
 <div class="row justify-content-center">
@@ -10,19 +15,35 @@
 		<h4 class="text-center pt-3"><span class="text-light"> Total:</span> <strong>{{ $total }} $ </strong></h4>
 		
 		<form class="form-horizontal" action="{{ route('postcheckout') }}" method = "post" id="checkout-form" >
+			{{csrf_field()}}
 			<div class="form-group ">
 			  <div class="col-sm-12">
-				<input type="text" class="form-control" id="name"  name ="name" placeholder="Enter your name">
+				<input type="text" class="form-control  @error('name') is-invalid @enderror" id="name"  name ="name" placeholder="Enter your name">
+				@error('name')
+                          <span class="invalid-feedback" role="alert">
+                              <strong>{{ $message }}</strong>
+                          </span>
+                      @enderror
 			  </div>
 			</div>
 			<div class="form-group">
 			  <div class="col-sm-12">
-				<input type="text" class="form-control" id="address" name="address" placeholder="Enter your addess">
+				<input type="text" class="form-control @error('address') is-invalid @enderror" id="address" name="address" placeholder="Enter your addess">
+				@error('address')
+                          <span class="invalid-feedback" role="alert">
+                              <strong>{{ $message }}</strong>
+                          </span>
+                      @enderror
 			  </div>
 			</div>
 			<div class="form-group">
 				<div class="col-sm-12">
-				  <input type="text" class="form-control" id="creditcardnumber" name="creditcardnumber" placeholder="Enter your shopping cart">
+				  <input type="text" class="form-control @error('creditcardnumber') is-invalid @enderror" id="creditcardnumber" name="creditcardnumber" placeholder="Enter your shopping cart">
+				  @error('creditcardnumber')
+                          <span class="invalid-feedback" role="alert">
+                              <strong>{{ $message }}</strong>
+                          </span>
+                      @enderror
 				</div>
 			  </div>
 			<div class="form-group">
